0765226c6cb4c13caf2484459c9bba867fa84340
Alessandra Chardon <achardon@ucla.edu>
2021-11-30 09:29:53 -0500

    commit tree 1422aeb1ae7703dddfa9a48295a35b53de056c24

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,12 +3,12 @@ class Shoe
     attr_accessor :brand, :color, :size, :material, :condition
 
     def initialize(brand)
-        @brand = brand
+        self.brand = brand
     end
 
     def cobble
         puts "Your shoe is as good as new!"
-        @condition = 'new'
+        self.condition = 'new'
     end
 
 end
